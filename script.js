afc2a46f87492f51673b96f3f386a824e4e4d873
EmberTSeal <trishaseal49@gmail.com>
2023-06-30 23:56:15 +0530

    inputting numbers and operator

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-let num1, num2, operator;
-let operandString = [];
+let num1 = '', num2 = '', operator = '';
+let operatorExist = false;
 
 const digits = Array.from(document.querySelector('#digits').children);
 const display = document.querySelector('#display');
+const operators = Array.from(document.querySelector('#operators').children);
 
-digits.forEach(digit => digit.addEventListener('click', function(){ 
-    operandString += digit.innerHTML
-    display.textContent = operandString ;
+
+digits.forEach(digit => digit.addEventListener('click', function () {
+    if (operatorExist === false) {
+        num1 += digit.innerHTML;
+    }
+    else {
+        num2 += digit.innerHTML;
+    }
+    display.textContent += digit.innerHTML;
 }));
+
+operators.forEach(operatorInArray =>
+    operatorInArray.addEventListener('click', function () {
+        if (operatorExist === false) {
+            operator = operatorInArray;
+            operatorExist = true;
+            display.textContent += ' ' + operatorInArray.innerHTML + ' ';
+        }
+    }));
 
 const clear = document.querySelector('#clear');
 
-clear.addEventListener('click', function(){
+clear.addEventListener('click', function () {
     operandString = '';
     display.textContent = '';
+    operatorExist = false;
 });
 
 let add = (a, b) => (a + b);
